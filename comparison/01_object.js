27449e2e2fe9208038af004795a00e74398f65fd
bablu kumar <bablukumar06800@gmail.com>
2025-09-04 22:45:52 +0530

    changes object

--- comparison/01_object.js
+++ comparison/01_object.js
@@ -28,29 +28,49 @@ tinderUser.email = "bablutechnical.com"
 const newobj = {...obj1,...obj2,...obj3}
 //console.log( newobj);
  
-const user = [
-  {
-  id: 1,
-  name: "bablu",
-  email: "bablu.com"
-  },
-  {
-  id: 1,
-  name: "bablu",
-  email: "bablu.com"
-  },
-  {
-  id: 1,
-  name: "bablu",
-  email: "bablu.com"
-  }
-]
-  user[1].email
-  console.log(tinderUser);
+// const user = [
+//   {
+//   id: 1,
+//   name: "bablu",
+//   email: "bablu.com"
+//   },
+//   {
+//   id: 1,
+//   name: "bablu",
+//   email: "bablu.com"
+//   },
+//   {
+//   id: 1,
+//   name: "bablu",
+//   email: "bablu.com"
+//   }
+// ]
+//   user[1].email
+//   console.log(tinderUser);
 
-  console.log(Object.keys(tinderUser));
-  console.log(Object.values(tinderUser));
-  console.log(Object.entries(tinderUser));
-  console.log(tinderUser.hasOwnProperty("name"));
+//   console.log(Object.keys(tinderUser));
+//   console.log(Object.values(tinderUser));
+//   console.log(Object.entries(tinderUser));
+//   console.log(tinderUser.hasOwnProperty("name"));
+
+
+// Destructuring in object
+
+const course = {
+  coureseName:"js hindi",
+  price:999,
+  courseInstructor:"bablu kumar",
+
+}
+console.log(course.price);
+
+const {courseInstructor: karanKumar} = course
+//console.log(courseInstructor);
+console.log(karanKumar);
+
+
+
+
+
   
   
