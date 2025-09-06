0e07355e9ff3c14edced1c64a1e025de97dc729c
bablu kumar <bablukumar06800@gmail.com>
2025-09-06 23:46:20 +0530

    scope

---
+++ comparison/function.js/scope.js
@@ -0,0 +1,22 @@
+// let a = 10;
+// var b = 20;
+// const c = 30;
+
+//console.log(a,b,c);
+let a = 500;
+
+if(true){
+  let a = 10;
+var b = 20;
+const c = 30;
+console.log("inner value" , a);
+
+
+}
+// console.log(a);
+// console.log(b);
+console.log(b);
+console.log(a);
+
+
+
