529b301181d166dcdb0f5f9fac2c71511128b363
bablu kumar <bablukumar06800@gmail.com>
2025-09-07 00:31:03 +0530

    changes

--- comparison/function.js/scope.js
+++ comparison/function.js/scope.js
@@ -15,8 +15,50 @@ console.log("inner value" , a);
 }
 // console.log(a);
 // console.log(b);
-console.log(b);
-console.log(a);
+// console.log(b);
+// console.log(a);
 
 
 
+function one(){
+  const username = "bablu";
+
+  function two(){
+    const website = "youtube.com";
+    console.log(username);
+    
+  }
+  two()
+} 
+one()
+
+
+ if(true){
+
+  const username = "bablu"
+  if(username === "bablu"){
+    const website = " youtube.com"
+    console.log(username  +  website);
+    
+  } 
+  
+ }
+
+
+console.log(one(5))
+
+ function one(num){
+  return num +1;
+
+ }
+
+
+ console.log(addTwo(8))
+ 
+ const addTwo = function(num){
+  return num+2;
+ }
+
+ 
+
+ 
