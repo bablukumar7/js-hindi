b71dd71d16f44d6535a8cc5a86042f2633d2ffba
bablu kumar <bablukumar06800@gmail.com>
2025-09-15 22:39:21 +0530

    loop

---
+++ comparison/function.js/Contril flow/iteration/three.js
@@ -0,0 +1,16 @@
+// while loop
+
+let index = 0;
+while(index <= 10){
+  console.log(`value of index is ${index}`);
+  index = index +2;
+  
+
+}
+
+let myArray = ['flash','superman','batman','ironman']
+let arr = 0;
+while(arr < myArray.length){
+  console.log( `value is ${myArray[arr]}`);
+  arr = arr +1;
+}
