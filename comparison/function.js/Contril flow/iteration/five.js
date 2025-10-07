35e1d704df6d204038a9dbf9f35c82c562211b34
bablu kumar <bablukumar06800@gmail.com>
2025-10-07 23:14:50 +0530

    iteration

---
+++ comparison/function.js/Contril flow/iteration/five.js
@@ -0,0 +1,13 @@
+let index = 0;
+while(index <= 10){
+  console.log(`value of index is ${index}`);
+  index = index +2;
+  
+}
+
+let myArray = [ 'flash' ,'superman' , 'batman']
+let arr = 0;
+while(arr < myArray.length){
+  console.log(`value is ${myArray[arr]}`);
+  arr = arr + 1;
+}
